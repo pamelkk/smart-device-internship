bfaec1f7aa9e12261ec5e2724e7c9bb45498c598
Rina <pamelkk@yandex.ru>
2021-01-28 01:40:02 +0300

    поправляет баги

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,29 @@
 const buttonOpen = document.querySelector(".page-header__menu");
 const menu = document.querySelector(".page-header__nav-wrapper");
+const bodyElement = document.querySelector(".page-body");
+const headerElement = document.querySelector(".page-header");
+const headerWrapperElement = document.querySelector(".page-header__nav-wrapper");
 
 menu.classList.add("page-header__nav-wrapper--inactive");
 buttonOpen.classList.remove("page-header__menu--opened");
+buttonOpen.classList.remove("page-header__menu--disabled");
+headerElement.classList.remove("page-header--active");
+bodyElement.classList.remove("page-body--inactive");
+headerWrapperElement.classList.remove("page-header__nav-wrapper--active");
+
 
 buttonOpen.addEventListener("click", function (evt) {
   if (!menu.classList.contains("page-header__nav-wrapper--inactive")) {
     menu.classList.add("page-header__nav-wrapper--inactive");
     buttonOpen.classList.remove("page-header__menu--opened");
+    bodyElement.classList.remove("page-body--inactive");
+    headerElement.classList.remove("page-header--active");
+    headerWrapperElement.classList.remove("page-header__nav-wrapper--active");
   } else {
     menu.classList.remove("page-header__nav-wrapper--inactive");
     buttonOpen.classList.add("page-header__menu--opened");
+    bodyElement.classList.add("page-body--inactive");
+    headerElement.classList.add("page-header--active");
+    headerWrapperElement.classList.add("page-header__nav-wrapper--active");
   }
 });
