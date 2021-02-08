298dd394449df96475975f0e60ba368e3ba80456
Rina <pamelkk@yandex.ru>
2021-02-08 14:23:55 +0300

    исправляет баги

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 const buttonOpen = document.querySelector(".page-header__menu");
 const menu = document.querySelector(".page-header__nav-wrapper");
 const headerElement = document.querySelector(".page-header");
+const mainElement = document.querySelector(".page-main");
 const headerWrapperElement = document.querySelector(".page-header__nav-wrapper");
 const pageElement = document.querySelector(".page");
 const pageHeaderNavElement = document.querySelector(".page-header__nav");
@@ -9,6 +10,8 @@ menu.classList.add("page-header__nav-wrapper--inactive");
 buttonOpen.classList.remove("page-header__menu--opened");
 buttonOpen.classList.remove("page-header__menu--disabled");
 headerElement.classList.remove("page-header--active");
+headerElement.classList.add("page-header--activate-bckg");
+mainElement.classList.add("page-main--inactive");
 headerWrapperElement.classList.remove("page-header__nav-wrapper--active");
 pageElement.classList.remove("page--inactive");
 pageHeaderNavElement.classList.remove("page-header__nav--active");
