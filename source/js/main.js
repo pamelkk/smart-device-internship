f1441b98ae9eac8f7f71d40453d13bbf2c7ab4d3
Rina <pamelkk@yandex.ru>
2021-03-01 00:42:07 +0300

    добавляет стили для таблетки и мобилки + джс

--- source/js/main.js
+++ source/js/main.js
@@ -1,36 +1,30 @@
-const buttonOpen = document.querySelector(".page-header__menu");
-const menu = document.querySelector(".page-header__nav-wrapper");
-const headerElement = document.querySelector(".page-header");
-const mainElement = document.querySelector(".page-main");
-const headerWrapperElement = document.querySelector(".page-header__nav-wrapper");
-const pageElement = document.querySelector(".page");
-const pageHeaderNavElement = document.querySelector(".page-header__nav");
+const buttonOpenAddress = document.querySelector(".page-footer__address-menu");
+const buttonOpenLists = document.querySelector(".page-footer__nav-lists-menu");
+const addressMenu = document.querySelector(".page-footer__address");
+const listsMenu = document.querySelector(".page-footer__nav-lists");
 
-menu.classList.add("page-header__nav-wrapper--inactive");
-buttonOpen.classList.remove("page-header__menu--opened");
-buttonOpen.classList.remove("page-header__menu--disabled");
-headerElement.classList.remove("page-header--active");
-headerElement.classList.add("page-header--activate-bckg");
-mainElement.classList.add("page-main--inactive");
-headerWrapperElement.classList.remove("page-header__nav-wrapper--active");
-pageElement.classList.remove("page--inactive");
-pageHeaderNavElement.classList.remove("page-header__nav--active");
+addressMenu.classList.remove("page-footer__address--opened");
+listsMenu.classList.remove("page-footer__nav-lists--opened");
+buttonOpenAddress.classList.remove("page-footer__address-menu--opened");
+buttonOpenLists.classList.remove("page-footer__nav-lists-menu--opened");
 
 
-buttonOpen.addEventListener("click", function (evt) {
-  if (!menu.classList.contains("page-header__nav-wrapper--inactive")) {
-    menu.classList.add("page-header__nav-wrapper--inactive");
-    buttonOpen.classList.remove("page-header__menu--opened");
-    headerElement.classList.remove("page-header--active");
-    headerWrapperElement.classList.remove("page-header__nav-wrapper--active");
-    pageElement.classList.remove("page--inactive");
-    pageHeaderNavElement.classList.remove("page-header__nav--active");
+buttonOpenAddress.addEventListener("click", function (evt) {
+  if (!addressMenu.classList.contains("page-footer__address--opened")) {
+    addressMenu.classList.remove("page-footer__address--opened");
+    buttonOpenAddress.classList.remove("page-footer__address-menu--opened");
   } else {
-    menu.classList.remove("page-header__nav-wrapper--inactive");
-    buttonOpen.classList.add("page-header__menu--opened");
-    headerElement.classList.add("page-header--active");
-    headerWrapperElement.classList.add("page-header__nav-wrapper--active");
-    pageElement.classList.add("page--inactive");
-    pageHeaderNavElement.classList.add("page-header__nav--active");
+    addressMenu.classList.add("page-footer__address--opened");
+    buttonOpenAddress.classList.add("page-footer__address-menu--opened");
+  }
+});
+
+buttonOpenLists.addEventListener("click", function (evt) {
+  if (!addressMenu.classList.contains("page-footer__address--opened")) {
+    listsMenu.classList.add("page-footer__nav-lists--opened");
+    buttonOpenLists.classList.add("page-footer__nav-lists-menu--opened");
+  } else {
+    listsMenu.classList.remove("page-footer__nav-lists--opened");
+    buttonOpenLists.classList.remove("page-footer__nav-lists-menu--opened");
   }
 });
